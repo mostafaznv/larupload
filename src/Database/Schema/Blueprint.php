77526fb8cfa497aa0d24fa30a1aa3f9caa5ec263
mostafaznv <mostafa.zeinivand@gmail.com>
2020-09-21 17:27:13 +0330

    cleanup and improvement on migration Blueprint

--- src/Database/Schema/Blueprint.php
+++ src/Database/Schema/Blueprint.php
@@ -9,53 +9,53 @@ use PDO;
 class Blueprint
 {
     /**
-     * Add upload columns to the table.
+     * Add upload columns to the table
      *
      * @param BlueprintIlluminate $table
-     * @param $name
+     * @param string $name
      * @param string $mode
      */
-    public static function columns(BlueprintIlluminate $table, $name, $mode = 'heavy')
+    public static function columns(BlueprintIlluminate $table, string $name, string $mode = 'heavy'): void
     {
-        $table->string("{$name}_file_name")->nullable();
+        $table->string("{$name}_file_name", 255)->nullable();
 
         if ($mode == 'heavy') {
-            $table->integer("{$name}_file_size")->unsigned()->nullable();
+            $table->unsignedInteger("{$name}_file_size")->nullable();
             $table->string("{$name}_file_type", 85)->nullable();
             $table->string("{$name}_file_mime_type", 85)->nullable();
-            $table->integer("{$name}_file_width")->nullable();
-            $table->integer("{$name}_file_height")->unsigned()->nullable();
-            $table->integer("{$name}_file_duration")->nullable();
+            $table->unsignedInteger("{$name}_file_width")->nullable();
+            $table->unsignedInteger("{$name}_file_height")->nullable();
+            $table->unsignedInteger("{$name}_file_duration")->nullable();
             $table->string("{$name}_file_dominant_color", 7)->nullable();
             $table->string("{$name}_file_format", 85)->nullable();
             $table->string("{$name}_file_cover", 85)->nullable();
         }
         else {
-            $table->{self::jsonable()}("{$name}_file_meta")->nullable();
+            $table->{self::jsonColumnType()}("{$name}_file_meta")->nullable();
         }
     }
 
     /**
-     * Drop upload columns.
+     * Drop upload columns
      *
      * @param BlueprintIlluminate $table
      * @param $name
      * @param string $mode
      */
-    public static function dropColumns(BlueprintIlluminate $table, $name, $mode = 'heavy')
+    public static function dropColumns(BlueprintIlluminate $table, string $name, string $mode = 'heavy'): void
     {
         $columns = static::getDefaultColumns($name, $mode);
         $table->dropColumn($columns);
     }
 
     /**
-     * Get a list of default columns.
+     * Get a list of default columns
      *
      * @param $name
      * @param string $mode
      * @return array
      */
-    public static function getDefaultColumns($name, $mode = 'heavy')
+    public static function getDefaultColumns(string $name, string $mode = 'heavy'): array
     {
         $columns = [
             "{$name}_file_name"
@@ -78,11 +78,11 @@ class Blueprint
     }
 
     /**
-     * Get jsonable column data type.
+     * Get json column data type
      *
      * @return string
      */
-    protected static function jsonable()
+    protected static function jsonColumnType(): string
     {
         return DB::connection()->getPdo()->getAttribute(PDO::ATTR_DRIVER_NAME) === 'mysql' && version_compare(DB::connection()->getPdo()->getAttribute(PDO::ATTR_SERVER_VERSION), '5.7.8', 'ge') ? 'json' : 'text';
     }
