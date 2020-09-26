28e025aec6a380e3e4da8bbb3fa707115ac82a8b
mostafaznv <mostafa.zeinivand@gmail.com>
2020-09-26 20:09:39 +0330

    throw exception on errors

--- src/Storage/FFMpeg.php
+++ src/Storage/FFMpeg.php
@@ -3,6 +3,7 @@
 namespace Mostafaznv\Larupload\Storage;
 
 use Exception;
+use Illuminate\Http\UploadedFile;
 use Symfony\Component\HttpFoundation\File\File;
 use Illuminate\Support\Facades\Storage;
 use Mostafaznv\Larupload\Helpers\Helper;
@@ -11,54 +12,54 @@ use Symfony\Component\Process\Process;
 class FFMpeg
 {
     /**
-     * Attached file.
+     * Attached file
      *
      * @var object
      */
     protected $file;
 
     /**
-     * Larupload configurations.
+     * Larupload configurations
      *
      * @var array
      */
     protected $config;
 
     /**
-     * Video Metadata.
+     * Video Metadata
      *
      * @var array
      */
     protected $meta = [];
 
     /**
-     * FFMPEG binary address.
+     * FFMPEG binary address
      *
      * @var string
      */
     protected $ffmpeg;
 
     /**
-     * FFProbe binary address.
+     * FFProbe binary address
      *
      * @var string
      */
     protected $ffprobe;
 
     /**
-     * Default scale size.
-     * we use this value if width and height both were undefined.
+     * Default scale size
+     * we use this value if width and height both were undefined
      *
      * @var integer
      */
     const DEFAULT_SCALE = 850;
 
     /**
-     * FFMpeg constructor.
+     * FFMpeg constructor
      *
-     * @param $file
+     * @param UploadedFile $file
      */
-    public function __construct($file)
+    public function __construct(UploadedFile $file)
     {
         $this->config = config('larupload');
         $this->file = $file;
@@ -74,42 +75,44 @@ class FFMpeg
     }
 
     /**
-     * Get Metadata from media file using ffprobe.
+     * Get Metadata from media file using ffprobe
      *
      * @return array
+     * @throws Exception
      */
-    public function getMeta()
+    public function getMeta(): array
     {
         if (empty($this->meta)) {
+            $path = $this->file->getRealPath();
             $meta = [
                 'width'    => null,
                 'height'   => null,
                 'duration' => null,
             ];
 
-            try {
-                $path = $this->file->getRealPath();
+            $cmd = $this->cmd("{$this->ffprobe} -i $path -loglevel quiet -show_format -show_streams -print_format json");
 
-                $cmd = $this->cmd("{$this->ffprobe} -i $path -loglevel quiet -show_format -show_streams -print_format json");
+            $process = new Process($cmd);
+            $process->setTimeout($this->config['ffmpeg-timeout']);
+            $process->run();
+            $output = $process->getOutput();
 
-                $process = new Process($cmd);
-                $process->setTimeout($this->config['ffmpeg-timeout']);
-                $process->run();
-                $output = $process->getOutput();
+            if ($process->isSuccessful()) {
+                $output = json_decode($output);
+                if ($output !== null) {
+                    $stream = $output->streams[0];
 
-                if ($process->isSuccessful()) {
-                    $output = json_decode($output);
-                    if ($output !== null) {
-                        $stream = $output->streams[0];
-
-                        $meta['width'] = (isset($stream->width)) ? (int)$stream->width : null;
-                        $meta['height'] = (isset($stream->height)) ? (int)$stream->height : null;
-                        $meta['duration'] = (int)$stream->duration;
-                    }
+                    $meta['width'] = isset($stream->width) ? (int)$stream->width : null;
+                    $meta['height'] = isset($stream->height) ? (int)$stream->height : null;
+                    $meta['duration'] = (int)$stream->duration;
+                }
+                else {
+                    $process->addErrorOutput('ffprobe output is null');
+                    throw new Exception($process->getErrorOutput());
                 }
             }
-            catch (Exception $exception) {
-                // do nothing
+            else {
+                throw new Exception($process->getErrorOutput());
             }
 
             $this->meta = $meta;
@@ -119,30 +122,22 @@ class FFMpeg
     }
 
     /**
-     * Capture screen shot from video file.
+     * Capture screen shot from video file
      *
      * @param $fromSecond
-     * @param $style
-     * @param $storage
-     * @param $saveTo
-     * @return bool
+     * @param array $style
+     * @param string $storage
+     * @param string $saveTo
+     * @throws Exception
      */
-    public function capture($fromSecond, $style, $storage, $saveTo)
+    public function capture($fromSecond, array $style, string $storage, string $saveTo): void
     {
         $width = isset($style['width']) ? $style['width'] : null;
         $height = isset($style['height']) ? $style['height'] : null;
+        $scale = $width ? $width : ($height ? $height : 850);
         $mode = isset($style['mode']) ? $style['mode'] : null;
         $saveTo = Storage::disk($storage)->path($saveTo);
-
-        if ($width) {
-            $scale = $width;
-        }
-        else if ($height) {
-            $scale = $height;
-        }
-        else {
-            $scale = 850;
-        }
+        $path = $this->file->getRealPath();
 
         if (is_null($fromSecond)) {
             $meta = $this->getMeta();
@@ -150,28 +145,19 @@ class FFMpeg
             $fromSecond = number_format($fromSecond, 1);
         }
 
-        try {
-            $path = $this->file->getRealPath();
-            if ($mode == 'crop') {
-                if ($width and $height) {
-                    $cmd = escapeshellcmd("{$this->ffmpeg} -ss $fromSecond -i $path -vframes 1 -filter scale=-1:$scale,crop=$width:$height");
-                }
-                else {
-                    $cmd = escapeshellcmd("{$this->ffmpeg} -ss $fromSecond -i $path -vframes 1 -filter scale=-1:$scale,crop=$scale:$scale");
-                }
+        if ($mode == 'crop') {
+            if ($width and $height) {
+                $cmd = escapeshellcmd("{$this->ffmpeg} -ss $fromSecond -i $path -vframes 1 -filter scale=-1:$scale,crop=$width:$height");
             }
             else {
-                $cmd = escapeshellcmd("{$this->ffmpeg} -ss $fromSecond -i $path -vframes 1 -filter scale=-1:$scale");
+                $cmd = escapeshellcmd("{$this->ffmpeg} -ss $fromSecond -i $path -vframes 1 -filter scale=-1:$scale,crop=$scale:$scale");
             }
-
-
-            return $this->run($cmd, $storage, $saveTo);
         }
-        catch (Exception $exception) {
-            // do nothing
+        else {
+            $cmd = escapeshellcmd("{$this->ffmpeg} -ss $fromSecond -i $path -vframes 1 -filter scale=-1:$scale");
         }
 
-        return false;
+        $this->run($cmd, $storage, $saveTo);
     }
 
     /**
@@ -180,50 +166,43 @@ class FFMpeg
      * @param $style
      * @param $storage
      * @param $saveTo
-     * @return bool
+     * @throws Exception
      */
-    public function manipulate($style, $storage, $saveTo)
+    public function manipulate($style, $storage, $saveTo): void
     {
         $width = isset($style['width']) ? $style['width'] : null;
         $height = isset($style['height']) ? $style['height'] : null;
         $mode = isset($style['mode']) ? $style['mode'] : null;
         $scale = $this->calculateScale($mode, $width, $height);
         $saveTo = Storage::disk($storage)->path($saveTo);
+        $path = $this->file->getRealPath();
 
-        try {
-            $path = $this->file->getRealPath();
-
-            if ($mode == 'crop') {
-                if ($scale) {
-                    $cmd = escapeshellcmd("{$this->ffmpeg} -i $path -vf scale=$scale,crop=$width:$height,setsar=1");
-                }
-                else {
-                    $cmd = escapeshellcmd("{$this->ffmpeg} -i $path -vf crop=$width:$height,setsar=1");
-                }
+        if ($mode == 'crop') {
+            if ($scale) {
+                $cmd = escapeshellcmd("{$this->ffmpeg} -i $path -vf scale=$scale,crop=$width:$height,setsar=1");
             }
             else {
-                $cmd = escapeshellcmd("{$this->ffmpeg} -i $path -vf scale=$scale,setsar=1");
+                $cmd = escapeshellcmd("{$this->ffmpeg} -i $path -vf crop=$width:$height,setsar=1");
             }
-
-            return $this->run($cmd, $storage, $saveTo);
         }
-        catch (Exception $exception) {
-            //
+        else {
+            $cmd = escapeshellcmd("{$this->ffmpeg} -i $path -vf scale=$scale,setsar=1");
         }
 
-        return false;
+        $this->run($cmd, $storage, $saveTo);
     }
 
     /**
-     * Stream - Generate HLS video from source file.
+     * Stream - Generate HLS video from source file
      *
      * @param array $styles
-     * @param $storage
-     * @param $basePath
-     * @param $fileName
+     * @param string $storage
+     * @param string $basePath
+     * @param string $fileName
      * @return bool
+     * @throws Exception
      */
-    public function stream(array $styles, $storage, $basePath, $fileName)
+    public function stream(array $styles, string $storage, string $basePath, string $fileName)
     {
         $playlist = "#EXTM3U\n#EXT-X-VERSION:3\n";
         $converted = [];
@@ -232,62 +211,44 @@ class FFMpeg
          * Generate multiple video qualities from uploaded video.
          */
         foreach ($styles as $name => $style) {
-            try {
-                $path = $this->file->getRealPath();
-                $width = $style['width'];
-                $height = $style['height'];
-                $audioBitRate = $this->shortNumberToInteger($style['bitrate']['audio']);
-                $videoBitRate = $this->shortNumberToInteger($style['bitrate']['video']);
-                $styleBasePath = "$basePath/$name-convert";
+            $path = $this->file->getRealPath();
+            $width = $style['width'];
+            $height = $style['height'];
+            $audioBitRate = $this->shortNumberToInteger($style['bitrate']['audio']);
+            $videoBitRate = $this->shortNumberToInteger($style['bitrate']['video']);
+            $styleBasePath = "$basePath/$name-convert";
 
-                Storage::disk('local')->makeDirectory($styleBasePath);
-                $saveTo = Storage::disk('local')->path("$styleBasePath/$name.mp4");
+            Storage::disk('local')->makeDirectory($styleBasePath);
+            $saveTo = Storage::disk('local')->path("$styleBasePath/$name.mp4");
 
-                $cmd = escapeshellcmd("{$this->ffmpeg} -y -i $path -s {$width}x{$height} -y -strict experimental -acodec aac -b:a $audioBitRate -ac 2 -ar 48000 -vcodec libx264 -vprofile main -g 48 -b:v $videoBitRate -threads 64");
-                $convertResult = $this->run($cmd, 'local', $saveTo);
+            $cmd = escapeshellcmd("{$this->ffmpeg} -y -i $path -s {$width}x{$height} -y -strict experimental -acodec aac -b:a $audioBitRate -ac 2 -ar 48000 -vcodec libx264 -vprofile main -g 48 -b:v $videoBitRate -threads 64");
+            $this->run($cmd, 'local', $saveTo);
 
-
-                if ($convertResult) {
-                    $converted[$name] = [
-                        'path'      => $styleBasePath,
-                        'file'      => $saveTo,
-                        'bandwidth' => $videoBitRate,
-                        'width'     => $width,
-                        'height'    => $height,
-                    ];
-                }
-                else {
-                    Storage::disk('local')->deleteDirectory($styleBasePath);
-                }
-            }
-            catch (Exception $e) {
-                // do nothing
-            }
+            $converted[$name] = [
+                'path'      => $styleBasePath,
+                'file'      => $saveTo,
+                'bandwidth' => $videoBitRate,
+                'width'     => $width,
+                'height'    => $height,
+            ];
         }
 
         /*
          * Convert generated videos to ts
          */
         foreach ($converted as $name => $value) {
-            try {
-                $m3u8 = 'chunk-list.m3u8';
-                $streamBasePath = "$basePath/$name";
-                Storage::disk($storage)->makeDirectory($streamBasePath);
-                $streamBasePath = Storage::disk($storage)->path($streamBasePath);
+            $m3u8 = 'chunk-list.m3u8';
+            $streamBasePath = "$basePath/$name";
+            Storage::disk($storage)->makeDirectory($streamBasePath);
+            $streamBasePath = Storage::disk($storage)->path($streamBasePath);
 
-                $cmd = escapeshellcmd("{$this->ffmpeg} -y -i {$value['file']} -hls_time 9 -hls_segment_filename :stream-path/file-sequence-%d.ts -hls_playlist_type vod :stream-path/$m3u8");
-                $streamResult = $this->streamRun($cmd, $storage, $streamBasePath);
+            $cmd = escapeshellcmd("{$this->ffmpeg} -y -i {$value['file']} -hls_time 9 -hls_segment_filename :stream-path/file-sequence-%d.ts -hls_playlist_type vod :stream-path/$m3u8");
+            $this->streamRun($cmd, $storage, $streamBasePath);
 
-                if ($streamResult) {
-                    $playlist .= "#EXT-X-STREAM-INF:BANDWIDTH={$value['bandwidth']},RESOLUTION={$value['width']}x{$value['height']}\n";
-                    $playlist .= "$name/$m3u8\n";
-                }
+            $playlist .= "#EXT-X-STREAM-INF:BANDWIDTH={$value['bandwidth']},RESOLUTION={$value['width']}x{$value['height']}\n";
+            $playlist .= "$name/$m3u8\n";
 
-                Storage::disk('local')->deleteDirectory($value['path']);
-            }
-            catch (Exception $e) {
-                Storage::disk('local')->deleteDirectory($value['path']);
-            }
+            Storage::disk('local')->deleteDirectory($value['path']);
         }
 
         if (count($converted)) {
@@ -299,14 +260,15 @@ class FFMpeg
     }
 
     /**
-     * Calculate scale.
+     * Calculate scale
      *
      * @param $mode
      * @param $width
      * @param $height
      * @return float|string
+     * @throws Exception
      */
-    protected function calculateScale($mode, $width, $height)
+    protected function calculateScale(string $mode, $width, $height)
     {
         $meta = $this->getMeta();
 
@@ -314,7 +276,7 @@ class FFMpeg
             if ($width >= $meta['width'] or $height >= $meta['height']) {
                 if ($meta['width'] >= $meta['height']) {
                     $scale = ceil(($meta['width'] * $height) / $meta['height']);
-                    //dd($scale, $width, $height, $meta);
+
                     if ($scale < $width) {
                         $scale = $width;
                     }
@@ -323,6 +285,7 @@ class FFMpeg
                 }
                 else {
                     $scale = ceil(($meta['height'] * $width) / $meta['width']);
+
                     if ($scale < $height) {
                         $scale = $height;
                     }
@@ -335,15 +298,7 @@ class FFMpeg
             }
         }
         else {
-            if ($width) {
-                $scale = "$width:-2";
-            }
-            else if ($height) {
-                $scale = "-2:$height";
-            }
-            else {
-                $scale = self::DEFAULT_SCALE . ':-2';
-            }
+            $scale = $width ? "$width:-2" : ($height ? "-2:$height" : (self::DEFAULT_SCALE . ':-2'));
         }
 
         return $scale;
@@ -353,12 +308,12 @@ class FFMpeg
      * Run ffmpeg command.
      * Handle local/non-local drivers
      *
-     * @param $cmd
-     * @param $storage
-     * @param $saveTo
-     * @return bool
+     * @param string $cmd
+     * @param string $storage
+     * @param string $saveTo
+     * @throws Exception
      */
-    protected function run($cmd, $storage, $saveTo)
+    protected function run(string $cmd, string $storage, string $saveTo): void
     {
         $driver = Helper::diskToDriver($storage);
 
@@ -369,8 +324,10 @@ class FFMpeg
             $process->run();
 
             if ($process->isSuccessful()) {
-                return true;
+                return;
             }
+
+            throw new Exception($process->getErrorOutput());
         }
         else {
             list($path, $name) = Helper::splitPath($saveTo);
@@ -390,23 +347,23 @@ class FFMpeg
                 Storage::disk($storage)->putFileAs($path, $file, $name);
                 @unlink($temp);
 
-                return true;
+                return;
             }
-        }
 
-        return false;
+            throw new Exception($process->getErrorOutput());
+        }
     }
 
     /**
-     * Run ffmpeg command for stream videos.
+     * Run ffmpeg command for stream videos
      * Handle local/non-local drivers
      *
-     * @param $cmd
-     * @param $storage
-     * @param $streamPath
-     * @return bool
+     * @param string $cmd
+     * @param string $storage
+     * @param string $streamPath
+     * @throws Exception
      */
-    protected function streamRun($cmd, $storage, $streamPath)
+    protected function streamRun(string $cmd, string $storage, string $streamPath): void
     {
         $driver = Helper::diskToDriver($storage);
 
@@ -417,14 +374,14 @@ class FFMpeg
             $process->setTimeout($this->config['ffmpeg-timeout']);
             $process->run();
 
-
             if ($process->isSuccessful()) {
-                return true;
+                return;
             }
+
+            throw new Exception($process->getErrorOutput());
         }
         else {
             list($path, $name) = Helper::splitPath($streamPath);
-
 
             $temp = $name . '-' . time();
 
@@ -448,21 +405,22 @@ class FFMpeg
                 }
 
                 Storage::disk('local')->deleteDirectory($temp);
-                return true;
-            }
-        }
 
-        return false;
+                return;
+            }
+
+            throw new Exception($process->getErrorOutput());
+        }
     }
 
     /**
-     * Convert short number formats to integer.
+     * Convert short number formats to integer
      * Example: 1M -> 1000000
      *
      * @param $number
      * @return int
      */
-    protected function shortNumberToInteger($number)
+    protected function shortNumberToInteger($number): int
     {
         $number = strtoupper($number);
 
@@ -478,16 +436,17 @@ class FFMpeg
         }
 
         $number = (float)$number * $units[$unit];
+
         return (int)$number;
     }
 
     /**
      * Make Normal CMD
      *
-     * @param $cmd
+     * @param string $cmd
      * @return array
      */
-    protected function cmd($cmd)
+    protected function cmd(string $cmd): array
     {
         return explode(' ', escapeshellcmd($cmd));
     }
