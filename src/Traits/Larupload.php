b2b74ada56ccae17739ed6e5c60b6bfe4b4723d5
mostafaznv <mostafa.zeinivand@gmail.com>
2023-03-18 10:44:36 +0330

    IDE friendly relations

--- src/Traits/Larupload.php
+++ src/Traits/Larupload.php
@@ -7,6 +7,10 @@ use Mostafaznv\Larupload\Concerns\LaruploadObservers;
 use Mostafaznv\Larupload\Concerns\LaruploadRelations;
 use Mostafaznv\Larupload\Concerns\LaruploadTransformers;
 
+/**
+ * @method static \Illuminate\Database\Eloquent\Relations\HasOne laruploadQueue()
+ * @method static \Illuminate\Database\Eloquent\Relations\HasMany laruploadQueues()
+ */
 trait Larupload
 {
     use BaseLarupload;
