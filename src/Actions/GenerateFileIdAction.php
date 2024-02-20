0ddc6750d80e8126f05c2bdc8f477cdf05b9c883
jlloret <jorgelloret@users.noreply.github.com>
2024-02-20 08:58:04 +0100

    Reformat code

--- src/Actions/GenerateFileIdAction.php
+++ src/Actions/GenerateFileIdAction.php
@@ -29,10 +29,10 @@ class GenerateFileIdAction
         }
 
         return match ($this->method) {
-            LaruploadSecureIdsMethod::ULID   => Str::ulid(),
-            LaruploadSecureIdsMethod::UUID   => Str::uuid(),
+            LaruploadSecureIdsMethod::ULID => Str::ulid(),
+            LaruploadSecureIdsMethod::UUID => Str::uuid(),
             LaruploadSecureIdsMethod::SQID => $this->sqid(),
-            default                          => $this->model->id,
+            default                        => $this->model->id,
         };
     }
 
