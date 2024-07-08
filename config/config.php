55f635095c63adeea9ea015770114e622df2ed09
mostafaznv <mostafa.zeinivand@gmail.com>
2024-07-08 15:04:19 +0330

    chore: add todo block above the storeOriginalFileName

--- config/config.php
+++ config/config.php
@@ -221,7 +221,7 @@ return [
     |
     | Set this to true in order to store the original file name in the database
     |
-    | todo - set it to true by default in the next major version
+    | todo - remove it and store original file name by default in the next major version
     */
 
     'store-original-file-name' => false,
