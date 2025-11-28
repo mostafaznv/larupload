010eb567de25eb441b58525b518373893f7ae2e3
mostafaznv <mostafa.zeinivand@gmail.com>
2025-11-29 00:14:01 +0330

    test: refactor `SetFileNameActionTest` by removing unused setup code

--- tests/Unit/Actions/SetFileNameActionTest.php
+++ tests/Unit/Actions/SetFileNameActionTest.php
@@ -1,37 +1,11 @@
 <?php
 
-use Carbon\Carbon;
-use Illuminate\Support\Facades\Storage;
-use Mostafaznv\Larupload\Actions\Attachment\CleanAttachmentAction;
 use Mostafaznv\Larupload\Actions\SetFileNameAction;
 use Mostafaznv\Larupload\Enums\LaruploadNamingMethod;
-use Mostafaznv\Larupload\Storage\Attachment;
-use Mostafaznv\Larupload\DTOs\Style\Output;
 use Mostafaznv\Larupload\Test\Support\LaruploadTestConsts;
 use function Spatie\PestPluginTestTime\testTime;
 
 
-beforeEach(function () {
-    $this->disk = 'public';
-
-    Storage::fake($this->disk);
-    $this->storage = Storage::disk($this->disk);
-
-    $this->attachment = Attachment::make('test_name')->disk($this->disk);
-    $this->attachment->id = 'test-id';
-    $this->attachment->folder = 'test-folder';
-    $this->attachment->nameKebab = 'test-name';
-    $this->attachment->output = Output::make(name: '284191.txt', originalName: 'file.txt');
-
-
-    $this->path = larupload_relative_path($this->attachment, $this->attachment->id);
-
-    $this->storage->makeDirectory($this->path);
-    $this->storage->put("$this->path/test.txt", 'Sample content');
-
-
-    $this->action = resolve(CleanAttachmentAction::class);
-});
 
 it('generates file name using hash method', function () {
     $result = SetFileNameAction::make(jpg(), LaruploadNamingMethod::HASH_FILE)->generate();
