5d983b0f35cecdd51fac9c5bb595aa52d8088d2f
mostafaznv <mostafa.zeinivand@gmail.com>
2025-12-05 14:21:27 +0330

    test: remove unused setup code in `OptimizeImageActionTest`

--- tests/Unit/Actions/OptimizeImageActionTest.php
+++ tests/Unit/Actions/OptimizeImageActionTest.php
@@ -1,34 +1,8 @@
 <?php
 
 use Mostafaznv\Larupload\Exceptions\InvalidImageOptimizerException;
-use Mostafaznv\Larupload\Larupload;
-use Mostafaznv\Larupload\Storage\Attachment;
-use Mostafaznv\Larupload\Test\Support\Enums\LaruploadTestModels;
 use Illuminate\Http\UploadedFile;
 use Mostafaznv\Larupload\Actions\OptimizeImageAction;
-
-
-beforeEach(function () {
-    $this->disk = 'public';
-
-    Queue::fake();
-    Storage::fake('s3');
-    Storage::fake($this->disk);
-
-    $this->attachment = Attachment::make('main_file');
-    $this->attachment->disk = $this->disk;
-    $this->attachment->localDisk = $this->disk;
-    $this->attachment->folder = 'test-folder';
-    $this->attachment->nameKebab = 'main-file';
-    $this->attachment->id = 'test-id';
-
-    $this->path = larupload_relative_path($this->attachment, $this->attachment->id);
-    $this->original = $this->path . '/' . Larupload::ORIGINAL_FOLDER;
-    $this->cover = $this->path . '/' . Larupload::COVER_FOLDER;
-
-    $this->model = LaruploadTestModels::HEAVY->instance();
-    $this->model->id = 52;
-});
 
 
 it('optimizes image successfully', function (UploadedFile $file) {
